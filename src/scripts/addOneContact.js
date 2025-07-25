1656f4a34950a591b5fb20413985e0702a256152
Oleksandr Palilov <opalilovit@gmail.com>
2025-07-26 00:46:59 +0300

    create generate and add one contacts

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,18 @@
-export const addOneContact = async () => {};
+import { faker } from '@faker-js/faker';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  try {
+    let existingsContacts = await readContacts();
+    const user = faker.helpers.multiple(createFakeContact, {
+      count: 1,
+    });
+    await writeContacts([...existingsContacts, ...user]);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,17 @@
-const generateContacts = async (number) => {};
+import { faker } from '@faker-js/faker';
 
-generateContacts(5);
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { readContacts } from '../utils/readContacts.js';
+
+const generateContacts = async (number) => {
+  try {
+    const users = faker.helpers.multiple(createFakeContact, { count: number });
+    let existingsContacts = await readContacts();
+    await writeContacts([...users, ...existingsContacts]);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+generateContacts(3);
